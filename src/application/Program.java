3fc87743467b9bb8541844efa42a183c73fc952f
FabioMattos2707 <fabio.matos2707@outlook.com>
2022-03-23 22:36:25 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,24 @@
 package application;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
 		
 		Department obj = new Department(1,"Books");
+		
 		Seller seller = new Seller(1,"Fabio","fabio@gmail.com",new Date(),5000.00,obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		
 		System.out.println(seller);
 
 	}
